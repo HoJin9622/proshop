a61619ccb4f0e1ba4252eb570af76251b3c1c74b
HoJin9622 <kiss0104040@gmail.com>
2020-12-28 23:08:25 +0900

    feat: Admin Order List

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -12,9 +12,12 @@ import {
   ORDER_LIST_MY_REQUEST,
   ORDER_LIST_MY_SUCCESS,
   ORDER_LIST_MY_FAIL,
+  ORDER_LIST_REQUEST,
+  ORDER_LIST_SUCCESS,
+  ORDER_LIST_FAIL,
 } from '../constants/orderConstants'
 
-export const createOrder = (order) => async (dispatch, getState) => {
+export const createOrder = order => async (dispatch, getState) => {
   try {
     dispatch({
       type: ORDER_CREATE_REQUEST,
@@ -48,7 +51,7 @@ export const createOrder = (order) => async (dispatch, getState) => {
   }
 }
 
-export const getOrderDetails = (id) => async (dispatch, getState) => {
+export const getOrderDetails = id => async (dispatch, getState) => {
   try {
     dispatch({
       type: ORDER_DETAILS_REQUEST,
@@ -147,6 +150,39 @@ export const listMyOrder = () => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: ORDER_LIST_MY_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+export const listOrders = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_LIST_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.get(`/api/orders`, config)
+
+    dispatch({
+      type: ORDER_LIST_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: ORDER_LIST_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
